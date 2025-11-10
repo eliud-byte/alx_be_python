458d50fb6f68fc6f9a6afe89f4a53ae85ae574e0
Eliud Mwaura <eliudmwaura101@gmail.com>
2025-11-10 12:59:22 +0300

    Create basic game code
    Create variables to hold user input.
    Create a print message that prints after the user
    input is done.

---
+++ simple_mad_libs_generator/simple_mad_libs_generator.py
@@ -0,0 +1,15 @@
+print("Enter a word to desctibe:")
+
+adjective1 = input("\tthe day: ")
+adjective2 = input("\tan animal: ")
+adjective3 = input("\tan animal: ")
+adjective4 = input("\tan experience: ")
+
+print("\n<<<<<<<<<<<<<<STORY>>>>>>>>>>>>>>>>>>\n")
+
+print(f"On a beautiful {adjective1} day, I went to the zoo. "
+      f"I saw a funny {adjective2} monkey swinging from the "
+      f"trees. Then, I spotted a majestic {adjective3} lion "
+      f"lounging in the sun. What a wild and {adjective4} "
+      f"experience!"
+      )
