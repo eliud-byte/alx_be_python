ef6c9d81d06e9e3a9d27ced819cfa004ed56b686
Eliud Mwaura <eliudmwaura101@gmail.com>
2025-11-11 09:15:01 +0300

    Add code to match input against data types

---
+++ match_case/matching_data_types.py
@@ -0,0 +1,9 @@
+value = input("Enter a value (number or string): ")
+
+match value:
+    case int():
+        print("You entered an integer:", value)
+    case str():
+        print("You entered a string:", value)
+    case _:
+        print("Invalid data type entered.")
