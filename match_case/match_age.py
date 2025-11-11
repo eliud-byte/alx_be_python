8cd5072380919a5811ee0654d44b0ef9df8c4ba5
Eliud Mwaura <eliudmwaura101@gmail.com>
2025-11-11 09:38:52 +0300

    Create code to verify voter eligibility
    Use match case statements match.
    Add a has_id function as a guard agains't teenagers.

---
+++ match_case/match_age.py
@@ -0,0 +1,11 @@
+age = int(input("Enter your age: "))
+
+match age:
+    case 18 | 19:
+        if age >= 18 and has_id(user):
+            # Guard using a function call
+            print("You are eligible to vote.")
+        else:
+            print("You need a valid ID to vote.")
+    case _:
+        print("You are not yet eligible to vote.")
