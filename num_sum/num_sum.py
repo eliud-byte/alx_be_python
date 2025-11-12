9bdffab06b0b2180903cb1e261e9e4a4a3dd757d
Eliud Mwaura <eliudmwaura101@gmail.com>
2025-11-12 10:41:43 +0300

    Refactor input handling and number summation logic
    For clarity and error handling

--- num_sum/num_sum.py
+++ num_sum/num_sum.py
@@ -4,16 +4,29 @@ This program:
     - creates a list of numbers in the range 1 - 100
     - calculates the sum of all numbers in the list.
 """
-# Take User input
-user_input = int(input("Enter any number between 1-100: "))
 
-# Make a list out of the number in user input range
-numbers = []
-for num in range(user_input):
-    numbers.append(num)
+while True:
+    # Catch ValueError
+    try:
+        # Take User input
+        user_input = int(input("Enter any number between 1-100: "))
+        
+        # Make sure that input is not more than 100 and run the program if not
+        if user_input <= 100:
 
-# Sum up the numbers in the list
-total = 0
-for n in numbers:
-    total += n
-print(total)
+            # Create a range out of the input and make a list out of numbers within
+            numbers = []
+            for num in range(user_input):
+                numbers.append(num)
+
+            # Sum up the numbers in the list
+            total = 0
+            for n in numbers:
+                total += n
+            print(total)
+            break       
+        else:
+            print("You number is over 100!!!")
+    
+    except ValueError:
+        print("Invalid input. Integers only!")
