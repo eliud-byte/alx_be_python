888ccf221e58e88cee8e5e30ee91c70bb8c712af
Eliud Mwaura <eliudmwaura101@gmail.com>
2025-11-13 15:19:04 +0300

    Reassign comments for relevance

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,18 +1,16 @@
 print("Hello User!!!")
 print("Enter a positive integer and I'll draw you an asterisk square!")
 
-# Prompt user for pattern size
 try:
-    integer = int(input("Enter the size of the pattern: "))
+    # Prompt user for pattern size
+    integer = int(input("Enter the size of the pattern: ")) 
     width = integer
-
+    
     while integer > 0:
         for w in range(width):
-            print("*", end="")
+            print("*", end="") # Draw the pattern
         integer -= 1
         print()
     
 except ValueError:
     print("Invalid entry!")
-
-# Draw the pattern
