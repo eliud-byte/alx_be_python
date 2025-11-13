11c5042ccd4e173ef48cd0190390b4be41243f92
Eliud Mwaura <eliudmwaura101@gmail.com>
2025-11-13 13:35:48 +0300

    Add indexing reference

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,11 +1,15 @@
 weather = ["sunny", "rainy", "cold"]
-current_weather = input("What's the weather like today? (sunny/rainy/cold): ")
+current_weather = str(input("What's the weather like today? (sunny/rainy/cold): "))
 
-if current_weather == "sunny":
-    print("Wear a t-shirt and sunglasses.")
-elif current_weather == "rainy":
-    print("Don't forget your umbrella and a raincoat.")
-elif current_weather == "cold":
-    print("Make sure to wear a warm coat and a scarf.")
-else:
-    print("Sorry, I don't have recommendations for this weather.")
+try:
+    if current_weather == weather[0]:
+        print("Wear a t-shirt and sunglasses.")
+    elif current_weather == weather[1]:
+        print("Don't forget your umbrella and a raincoat.")
+    elif current_weather == weather[2]:
+        print("Make sure to wear a warm coat and a scarf.")
+    else:
+        print("Sorry, I don't have recommendations for this weather.")
+
+except ValueError:
+    print("I don't recognise that! Please enter valid weather.")
