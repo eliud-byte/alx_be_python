4212c7cbe14ced4203a241ea4314e6cf032566fa
Eliud Mwaura <eliudmwaura101@gmail.com>
2025-11-21 13:25:34 +0300

    Improve input prompts for adding and removing items

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -14,13 +14,13 @@ def main():
 
         if choice == '1':
             # Prompt for and add an item
-            item = input("\nWhat would you like to add: ")
+            item = input("Enter the item to add: ")
             shopping_list.append(item)
             print(f"{item} added to shopping list.")
 
         elif choice == '2':
             # Prompt for and remove an item
-            item = input("\nWhat would you like to remove: ")
+            item = input("Enter the item to remove: ")
             if  item in shopping_list:
                 shopping_list.remove(item)
                 print(f"{item} removed from shopping list.")
