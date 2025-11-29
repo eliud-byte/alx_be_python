c99c87a0ed4e1068ada916eaf88f34219d066601
Eliud Mwaura <eliudmwaura101@gmail.com>
2025-11-29 22:07:54 +0300

    Convert inputs into floats

--- programming_paradigm/main-1.py
+++ programming_paradigm/main-1.py
@@ -7,8 +7,8 @@ def main():
         sys.exit(1)
 
     try:
-        numerator = float(sys.argv[1])
-        denominator = float(sys.argv[2])
+        numerator = sys.argv[1]
+        denominator = sys.argv[2]
     
     except ValueError:
         print("Error: Please enter numeric values only.")
--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -2,7 +2,8 @@ def safe_divide(numerator, denominator):
     try:
         if denominator == 0:
             return "Error: Cannot divide by zero."
-        return float(numerator / denominator)
+        division = (float(numerator) / float(denominator))
+        return division
         
     except TypeError:
         return "Error: Inputs must be numeric types." 
