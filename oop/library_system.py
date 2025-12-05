9aee10f9309833c5631a30daba46e197a638feb8
Eliud Mwaura <eliudmwaura101@gmail.com>
2025-12-05 17:44:37 +0300

    feat: Implement core Library system using Inheritance and Composition
    This commit introduces the necessary class structure for the library
    system, demonstrating fundamental Object-Oriented Programming (OOP) concepts.
    
    Key implementations:
    
    - Defined the 'Book' base class.
    - Implemented 'EBook' and 'PrintBook' derived classes, showcasing
    **Inheritance** via super().
    - Implemented the 'Library' class, demonstrating **Composition** by
    managing a collection of Book instances.
    - Split definitions and usage into 'library_system.py' and 'main.py'.

---
+++ oop/library_system.py
@@ -0,0 +1,58 @@
+class Book:
+    """Base class for all books in the library."""
+    def __init__(self, title: str, author: str):
+        self.title = title
+        self.author = author
+
+    def get_details(self) -> str:
+        """Returns basic book details."""
+        #return f"Title: {self.title}, Author {self.author}"
+        return f"{self.__class__.__name__}: {self.title} by {self.author}"
+
+class EBook(Book):
+    """Represents an electronic book."""
+    def __init__(self, title: str, author: str, file_size: int):
+        super().__init__(title, author)
+        self.file_size = file_size
+
+    def get_details(self) -> str:
+        """Returns details specific to the EBook."""
+        base_details = super().get_details()
+        return f"{base_details}, File Size: {self.file_size}KB"
+
+class PrintBook(Book):
+    """Represents a physical printed book."""
+    def __init__(self, title: str, author: str, page_count: int):
+        super().__init__(title, author)
+        self.page_count = page_count
+
+    def get_details(self) -> str:
+        """Returns details specific to the PrintBook."""
+        base_details = super().get_details()
+        return f"{base_details}, Page Count: {self.page_count}"
+
+class Library():
+    """Manages a collection of Book, EBook, and PrintBook instances"""
+    def __init__(self):
+        self.books = []
+    
+    def add_book(self, book: Book):
+        """Adds a Book, EBook, or PrintBook instance to the library's collection."""
+        if isinstance(book, Book):
+            self.books.append(book)
+            print(f"Added '{book.title}' to the library")    
+        else:
+            print("Error: Item is not a valid Book type.")
+
+    def list_books(self):
+        """Prints details of every book in the library."""
+        print("\n--- Current Library Collection ---")
+        if not self.books:
+            print("The library is currently emply.")
+            return
+        
+        
+        for book in self.books:
+            print(book.get_details())
+
+        print("-------------------------------------------------------")
